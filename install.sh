8d891113c0c549251ea0fbe5ef6ba98431215d17
Baeumert <30773530+Baeumert@users.noreply.github.com>
2025-08-02 18:02:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@
 docker compose pull
 mkdir -p /docker/homeassistant/data/mqtt5/config/
 sudo cp mqtt5/config/mosquitto.conf /docker/homeassistant/data/mqtt5/config/mosquitto.conf
-sudo touch /docker/homeassistant/data/mqtt5/config/pwfile
-sudo chmod 0700 /docker/homeassistant/data/mqtt5/config/pwfile
 sudo chown root -R /docker/homeassistant/data/mqtt5/
 sudo chgrp docker -R /docker/homeassistant/data/mqtt5/
+sudo touch /docker/homeassistant/data/mqtt5/config/pwfile
+sudo chmod 0700 /docker/homeassistant/data/mqtt5/config/pwfile
+sudo chown root -R /docker/homeassistant/data/mqtt5/config/pwfile
+sudo chgrp root -R /docker/homeassistant/data/mqtt5/config/pwfile
 
 
 
