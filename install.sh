61c289b2931ca52954ac4aa979f1ab27ab56dfaa
Baeumert <30773530+Baeumert@users.noreply.github.com>
2025-08-02 17:44:29 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,12 @@
 
 #Script for install ang configuring HomeAssitant with AI
 docker compose pull
+mkdir -p /docker/homeassistant/data/mqtt5/config/
+sudo cp mqtt5/config/mosquitto.conf /docker/homeassistant/data/mqtt5/config/mosquitto.conf
+sudo touch /docker/homeassistant/data/mqtt5/config/pwfile
+sudo chown root -R /docker/homeassistant/data/mqtt5/
+sudo chgrp docker -R /docker/homeassistant/data/mqtt5/
 
-cp mqtt5/config/mosquitto.conf /docker/homeassistant/data/mqtt5/config/mosquitto.conf
-touch /docker/homeassistant/data/mqtt5/config/pwfile
 
 echo "Please enter a Secure Password for the MQTT-User: homassistant"
 read -p "MQTT Password for HomeAssitant" hapassword
@@ -13,9 +16,6 @@ read -p "MQTT Password for Zigbee2MQTT"  z2mpassword
 echo "Please enter a Secure Password for the MQTT-User: iot"
 read -p "MQTT Password for IoT Devices"  iotpassword
 
-docker -it exec mqtt5 touch /mosquitto/config/pwfile
-docker -it exec mqtt5 chown root /mosquitto/config/pwfile
-docker -it exec mqtt5 chgrp docker /mosquitto/config/pwfile
 docker -it exec mqtt5 mosquitto_passwd /mosquitto/config/pwfile homassistant
 docker -it exec mqtt5 mosquitto_passwd /mosquitto/config/pwfile zigbee2mqtt
 docker -it exec mqtt5 mosquitto_passwd /mosquitto/config/pwfile iot
