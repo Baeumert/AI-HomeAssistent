198627de908bd66435020f97960e61d22ab15983
Baeumert <30773530+Baeumert@users.noreply.github.com>
2025-08-02 14:43:46 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+#Script for install ang configuring HomeAssitant with AI
+
+echo "Please enter a Secure Password for the MQTT-User: homassistant"
+read -p "MQTT Password for HomeAssitant" hapassword
+echo "Please enter a Secure Password for the MQTT-User: zigbee2mqtt"
+read -p "MQTT Password for Zigbee2MQTT"  z2mpassword
+echo "Please enter a Secure Password for the MQTT-User: iot"
+read -p "MQTT Password for IoT Devices"  iotpassword
+
+docker -it exec mqtt5 touch /mosquitto/config/pwfile
+docker -it exec mqtt5 chown root /mosquitto/config/pwfile
+docker -it exec mqtt5 chgrp docker /mosquitto/config/pwfile
+docker -it exec mqtt5 mosquitto_passwd /mosquitto/config/pwfile homassistant
+docker -it exec mqtt5 mosquitto_passwd /mosquitto/config/pwfile zigbee2mqtt
+docker -it exec mqtt5 mosquitto_passwd /mosquitto/config/pwfile iot
